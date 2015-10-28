05ba6cddfc7c09999aa493b2b5245d3f1913dc8f
Adam Caron <adamwcaron@gmail.com>
2015-10-28 15:47:05 -0600

    Test individual initializor methods

--- test/spaceTime_test.js
+++ test/spaceTime_test.js
@@ -4,72 +4,82 @@ const assert = chai.assert;
 const ST     = require('../lib/spacetime');
 const $      = require('jquery');
 
-describe('spaceTime functionality', function () {
-    it('should have properties', function () {
-        let canvas = { width: 800, height: 400 };
-        let game   = { asteroidQuantity: 5, alienQuantity: 2 };
+describe('SpaceTime functionality', function () {
 
-        let spaceTime = new ST(canvas, null, game);
+    describe('Initial state of the universe', function () {
 
-        assert.equal(spaceTime.asteroids.length, 5);
-        assert.equal(spaceTime.aliens.length, 2);
-        assert.equal(spaceTime.lasers.length, 0);
-        assert.equal(spaceTime.alienLasers.length, 0);
-        assert.equal(spaceTime.shields.length, 0);
-        assert.equal(spaceTime.mines.length, 0);
+        it('has properties', function () {
+            let canvas = { width: 800, height: 400 };
+            let game   = { asteroidQuantity: 5, alienQuantity: 2 };
+
+            let spaceTime = new ST(canvas, null, game);
+
+            assert.equal(spaceTime.asteroids.length, 5);
+            assert.equal(spaceTime.aliens.length, 2);
+            assert.equal(spaceTime.lasers.length, 0);
+            assert.equal(spaceTime.alienLasers.length, 0);
+            assert.equal(spaceTime.shields.length, 0);
+            assert.equal(spaceTime.mines.length, 0);
+        });
+
+
+        it('initializes a bunch of asteroids', function () {
+            let canvas    = { width: 800, height: 400 };
+            let game      = { asteroidQuantity: null, alienQuantity: null };
+            let spaceTime = new ST(canvas, null, game);
+            let numberOfAsteroids = 15;
+
+            spaceTime.initialAsteroids(numberOfAsteroids);
+
+            assert.equal(spaceTime.asteroids.length, numberOfAsteroids);
+        });
+
+        it('initializes some aliens', function () {
+            let canvas    = { width: 800, height: 400 };
+            let game      = { asteroidQuantity: null, alienQuantity: null };
+            let spaceTime = new ST(canvas, null, game);
+            let numberOfAliens = 2;
+
+            spaceTime.initialAliens(numberOfAliens);
+
+            assert.equal(spaceTime.aliens.length, numberOfAliens);
+        });
     });
 
-    it('initializes a bunch of asteroids', function () {
-        let canvas    = { width: 800, height: 400 };
-        let game      = { asteroidQuantity: 5, alienQuantity: 2 };
-        let spaceTime = new ST(canvas, null, game);
-        let numberOfAsteroids = 15;
+    describe('weapons & such', function () {
 
-        spaceTime.initialAsteroids(numberOfAsteroids);
+        it('fires lasers', function () {
+            let canvas    = { width: 800, height: 400 };
+            let game      = { asteroidQuantity: 5, alienQuantity: 2 };
+            let spaceTime = new ST(canvas, null, game);
 
-        assert(spaceTime.asteroids.length, numberOfAsteroids);
-    });
+            spaceTime.fireLaser();
+            spaceTime.fireLaser();
+            spaceTime.fireLaser();
 
-    it('initializes some aliens', function () {
-        let canvas    = { width: 800, height: 400 };
-        let game      = { asteroidQuantity: 5, alienQuantity: 2 };
-        let spaceTime = new ST(canvas, null, game);
-        let numberOfAliens = 2;
+            assert.equal(spaceTime.lasers.length, 3)
+        });
 
-        spaceTime.initialAliens(numberOfAliens);
+        it('activates shields', function () {
+            let canvas    = { width: 800, height: 400 };
+            let game      = { asteroidQuantity: 5, alienQuantity: 2 };
+            let spaceTime = new ST(canvas, null, game);
 
-        assert(spaceTime.aliens.length, numberOfAliens);
-    });
+            spaceTime.activeShield();
 
-    it('fires lasers', function () {
-        let canvas    = { width: 800, height: 400 };
-        let game      = { asteroidQuantity: 5, alienQuantity: 2 };
-        let spaceTime = new ST(canvas, null, game);
+            assert.equal(spaceTime.shields.length, 1)
+        });
 
-        spaceTime.fireLaser();
+        it('drops mines', function () {
+            let canvas    = { width: 800, height: 400 };
+            let game      = { asteroidQuantity: 5, alienQuantity: 2 };
+            let spaceTime = new ST(canvas, null, game);
 
-        assert(spaceTime.lasers.length, 1)
-    });
+            spaceTime.layMine();
+            spaceTime.layMine();
+            spaceTime.layMine();
 
-    it('activates shields', function () {
-        let canvas    = { width: 800, height: 400 };
-        let game      = { asteroidQuantity: 5, alienQuantity: 2 };
-        let spaceTime = new ST(canvas, null, game);
-
-        spaceTime.activeShield();
-
-        assert(spaceTime.shields.length, 1)
-    });
-
-    it('drops mines', function () {
-        let canvas    = { width: 800, height: 400 };
-        let game      = { asteroidQuantity: 5, alienQuantity: 2 };
-        let spaceTime = new ST(canvas, null, game);
-
-        spaceTime.layMine();
-        spaceTime.layMine();
-        spaceTime.layMine();
-
-        assert(spaceTime.mines.length, 3)
+            assert.equal(spaceTime.mines.length, 3)
+        });
     });
 });
