8a09ed496d5ee77816c16c18f1d0177239269129
Adam Caron <adamwcaron@gmail.com>
2015-10-28 14:28:18 -0600

    Test that drawAll delegates to other methods

--- test/spaceTime_test.js
+++ test/spaceTime_test.js
@@ -6,14 +6,8 @@ const $      = require('jquery');
 
 describe('space-time functionality', function () {
     it('should have properties', function () {
-        let canvas = {
-                        width: 800,
-                        height: 500
-                    };
-        let game   = {
-                        asteroidQuantity: 5,
-                        alienQuantity: 2
-                    };
+        let canvas = { width: 800, height: 400 };
+        let game   = { asteroidQuantity: 5, alienQuantity: 2 };
 
         let spaceTime = new ST(canvas, null, game);
 
@@ -23,5 +17,27 @@ describe('space-time functionality', function () {
         assert.equal(spaceTime.alienLasers.length, 0);
         assert.equal(spaceTime.shields.length, 0);
         assert.equal(spaceTime.mines.length, 0);
+    });
+
+    it('draws all the objects', function () {
+        it('should delegate to all the draw methods', function () {
+            let canvas    = { width: 800, height: 400 };
+            let game      = { asteroidQuantity: 5, alienQuantity: 2 };
+            let spaceTime = new ST(canvas, null, game);
+
+            spaceTime.drawAll();
+
+            assert(spaceTime.detectSides.calledOnce);
+            assert(spaceTime.ship.moveShip().draw.calledOnce);
+            assert(spaceTime.drawAsteroids.calledOnce);
+            assert(spaceTime.drawAliens.calledOnce);
+            assert(spaceTime.detectShipCollision.calledOnce);
+            assert(spaceTime.drawLasers.calledOnce);
+            assert(spaceTime.drawAlienLasers.calledOnce);
+            assert(spaceTime.drawMines.calledOnce);
+            assert(spaceTime.drawShield.calledOnce);
+            assert(spaceTime.game.updateScore.calledOnce);
+            assert(spaceTime.game.updateLives.calledOnce);
+        });
     });
 });
