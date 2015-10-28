73c7a53f39a4cb16a45564ec2e7f1a9ec1a1234e
Adam Caron <adamwcaron@gmail.com>
2015-10-28 14:49:00 -0600

    Test initial asteroids and remove 'drawAll' tests

--- test/spaceTime_test.js
+++ test/spaceTime_test.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 const ST     = require('../lib/spacetime');
 const $      = require('jquery');
 
-describe('space-time functionality', function () {
+describe('spaceTime functionality', function () {
     it('should have properties', function () {
         let canvas = { width: 800, height: 400 };
         let game   = { asteroidQuantity: 5, alienQuantity: 2 };
@@ -19,25 +19,14 @@ describe('space-time functionality', function () {
         assert.equal(spaceTime.mines.length, 0);
     });
 
-    it('draws all the objects', function () {
-        it('should delegate to all the draw methods', function () {
-            let canvas    = { width: 800, height: 400 };
-            let game      = { asteroidQuantity: 5, alienQuantity: 2 };
-            let spaceTime = new ST(canvas, null, game);
+    it('initializes a bunch of asteroids', function () {
+        let canvas    = { width: 800, height: 400 };
+        let game      = { asteroidQuantity: 5, alienQuantity: 2 };
+        let spaceTime = new ST(canvas, null, game);
+        let numberOfAsteroids = 15;
 
-            spaceTime.drawAll();
+        spaceTime.initialAsteroids(numberOfAsteroids);
 
-            assert(spaceTime.detectSides.calledOnce);
-            assert(spaceTime.ship.moveShip().draw.calledOnce);
-            assert(spaceTime.drawAsteroids.calledOnce);
-            assert(spaceTime.drawAliens.calledOnce);
-            assert(spaceTime.detectShipCollision.calledOnce);
-            assert(spaceTime.drawLasers.calledOnce);
-            assert(spaceTime.drawAlienLasers.calledOnce);
-            assert(spaceTime.drawMines.calledOnce);
-            assert(spaceTime.drawShield.calledOnce);
-            assert(spaceTime.game.updateScore.calledOnce);
-            assert(spaceTime.game.updateLives.calledOnce);
-        });
+        assert(spaceTime.asteroids.length, numberOfAsteroids);
     });
 });
