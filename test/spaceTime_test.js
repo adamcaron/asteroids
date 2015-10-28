bb7f94d1edbf55643440d08ab668e427f136c4e1
Adam Caron <adamwcaron@gmail.com>
2015-10-28 14:08:51 -0600

    Test spaceTime's initial properties

--- test/spaceTime_test.js
+++ test/spaceTime_test.js
@@ -1,21 +1,27 @@
-const chai = require('chai');
+"use strict";
+const chai   = require('chai');
 const assert = chai.assert;
-const ST = require('../lib/spacetime');
-const $ = require('jquery');
+const ST     = require('../lib/spacetime');
+const $      = require('jquery');
 
-describe('my test suite', function () {
-    it('should work', function () {
-        assert(true);
-    });
+describe('space-time functionality', function () {
+    it('should have properties', function () {
+        let canvas = {
+                        width: 800,
+                        height: 500
+                    };
+        let game   = {
+                        asteroidQuantity: 5,
+                        alienQuantity: 2
+                    };
 
-    it('should add two plus two', function () {
-        let four = 2 + 2;
-        assert.equal(four,4);
-    });
+        let spaceTime = new ST(canvas, null, game);
 
-    it('should be deep equal', function (){
-        let x = [1,2,3,];
-        assert.deepEqual(x,[1,2,3]);
-        //Deep equal checks order and content of arrays
+        assert.equal(spaceTime.asteroids.length, 5);
+        assert.equal(spaceTime.aliens.length, 2);
+        assert.equal(spaceTime.lasers.length, 0);
+        assert.equal(spaceTime.alienLasers.length, 0);
+        assert.equal(spaceTime.shields.length, 0);
+        assert.equal(spaceTime.mines.length, 0);
     });
 });
