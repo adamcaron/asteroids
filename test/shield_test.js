60de948e11d55ed11fa82657058aaac9166ff27a
Alex Tideman <alex.tideman@gmail.com>
2015-10-28 14:15:50 -0600

    add tests for all objects

---
+++ test/shield_test.js
@@ -0,0 +1,22 @@
+'use strict';
+const chai = require('chai');
+const assert = chai.assert;
+const Shield = require('../lib/shield');
+const Ship = require('../lib/ship');
+const $ = require('jquery');
+
+describe('shield functionality', function () {
+    it('testing should work', function () {
+        assert(true);
+    });
+
+    it('should have properties', function () {
+        let ship = new Ship(10, 30, 20, 10);
+        let shield = new Shield(ship);
+        assert.equal(shield.x,10);
+        assert.equal(shield.y,30);
+        assert.equal(shield.radius,15);
+
+    });
+
+});
